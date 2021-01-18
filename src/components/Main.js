5554d37d280e701981633ce0cae591f399b90e97
austrisD <austris.daugulis@gmail.com>
2021-01-18 16:31:58 +0200

    Designe_done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,78 +1,84 @@
-import React, { useState } from "react";
+import React from "react";
 import Arrow_left from "../assets/arrow__left.svg";
 import Footer from "./Footer";
 import CheckBoxActive from "../assets/checkbox__active.svg";
 
-const Main = () => {
-  const [IsCheckBoxClicked, setIsCheckBoxClicked] = useState(false);
-  const [IsEmailInputActive, setEmailInputActive] = useState(false);
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
 
-  return (
-    <>
-      <main className="main">
-        <div className="main__heading">
-          <h2>Subscribe to newsletter</h2>
-          <p>
-            Subscribe to our newsletter and get 10% discount on pineapple
-            glasses.
-          </p>
-        </div>
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-        <form method="post" className="subscribe__form" target="self">
-          <label
-            htmlFor="email"
-            className="form__email"
-            style={
-              IsEmailInputActive === true
-                ? { boxShadow: "0px 30px 40px 0px #1317200f" }
-                : { boxShadow: "none" }
-            }
-          >
-            <input
-              type="email"
-              name="email"
-              id="form_email"
-              className="email__input"
-              placeholder="Type your email address here…"
-              onFocus={() => {
-                setEmailInputActive(true);
-              }}
-            />
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
 
-            <button
-              type="submit"
-              className="form__submit"
-              onClick={(event) => {
-                event.preventDefault();
-              }}
-            >
-              <Arrow_left className="btn__svg" />
-            </button>
-          </label>
-
-          <div className="form__checkbox">
-            <label className="checkbox__edit">
-              {IsCheckBoxClicked === true ? <CheckBoxActive /> : ""}
-              <input
-                type="checkbox"
-                className="checkbox"
-                onClick={(event) => {
-                  event.target.checked === true
-                    ? setIsCheckBoxClicked(true)
-                    : setIsCheckBoxClicked(false);
-                }}
-              ></input>
-            </label>
-            I agree to
-            <a href="#" className="termsLink">
-              terms of service
-            </a>
+  handleSubmit(event) {
+    alert("A name was submitted: " + this.state.value);
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <>
+        <main className="main">
+          <div className="main__heading">
+            <h2>Subscribe to newsletter</h2>
+            <p>
+              Subscribe to our newsletter and get 10% discount on pineapple
+              glasses.
+            </p>
           </div>
-        </form>
-        <Footer />
-      </main>
-    </>
-  );
-};
+
+          <form method="post" className="subscribe__form" target="self">
+            <label
+              htmlFor="email"
+              className="form__email"
+              // style={
+              //   IsEmailInputActive === true
+              //     ? { boxShadow: "0px 30px 40px 0px #1317200f" }
+              //     : { boxShadow: "none" }
+              // }
+            >
+              <input
+                type="email"
+                name="email"
+                id="form_email"
+                className="email__input"
+                placeholder="Type your email address here…"
+                required
+              />
+
+              <button
+                type="submit"
+                className="form__submit"
+         
+              >
+                <Arrow_left className="btn__svg" />
+              </button>
+            </label>
+
+            <div className="form__checkbox">
+              <label className="checkbox__edit">
+                <input
+                  type="checkbox"
+                  className="checkbox"
+      
+                ></input>
+              </label>
+              I agree to
+              <a href="#" className="termsLink">
+                terms of service
+              </a>
+            </div>
+          </form>
+          <Footer />
+        </main>
+      </>
+    );
+  }
+}
 
 export default Main;
