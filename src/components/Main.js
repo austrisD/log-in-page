cec2c92b420398e7f110ac8e27c47c626bcb3ba3
austrisd <austris.daugulis@gmail.com>
2021-02-16 13:52:35 +0200

    conflickt fix

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from "react";
-=======
 import React from "react";
->>>>>>> 5554d37d280e701981633ce0cae591f399b90e97
 import Arrow_left from "../assets/arrow__left.svg";
 import CheckBoxActiveIcon from "../assets/checkbox__active.svg";
 import Subscribed from "./Subscribed";
 
-<<<<<<< HEAD
 export default function Main() {
   const [Email, setEmail] = useState("");
   const [CheckBoxActive, setCheckBoxActive] = useState(false);
@@ -146,84 +142,3 @@ export default function Main() {
     </>
   );
 }
-=======
-class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: "" };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
-  handleSubmit(event) {
-    alert("A name was submitted: " + this.state.value);
-    event.preventDefault();
-  }
-  render() {
-    return (
-      <>
-        <main className="main">
-          <div className="main__heading">
-            <h2>Subscribe to newsletter</h2>
-            <p>
-              Subscribe to our newsletter and get 10% discount on pineapple
-              glasses.
-            </p>
-          </div>
-
-          <form method="post" className="subscribe__form" target="self">
-            <label
-              htmlFor="email"
-              className="form__email"
-              // style={
-              //   IsEmailInputActive === true
-              //     ? { boxShadow: "0px 30px 40px 0px #1317200f" }
-              //     : { boxShadow: "none" }
-              // }
-            >
-              <input
-                type="email"
-                name="email"
-                id="form_email"
-                className="email__input"
-                placeholder="Type your email address here…"
-                required
-              />
-
-              <button
-                type="submit"
-                className="form__submit"
-         
-              >
-                <Arrow_left className="btn__svg" />
-              </button>
-            </label>
-
-            <div className="form__checkbox">
-              <label className="checkbox__edit">
-                <input
-                  type="checkbox"
-                  className="checkbox"
-      
-                ></input>
-              </label>
-              I agree to
-              <a href="#" className="termsLink">
-                terms of service
-              </a>
-            </div>
-          </form>
-          <Footer />
-        </main>
-      </>
-    );
-  }
-}
-
-export default Main;
->>>>>>> 5554d37d280e701981633ce0cae591f399b90e97
